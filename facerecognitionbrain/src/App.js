6da48cf420dd598b8b80b64761453ff7420b3f68
Sara Moad <102942360+SaraMoad@users.noreply.github.com>
2023-02-17 12:22:32 +1100

    background customization

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -2,14 +2,19 @@ import './App.css';
 import Navigation from './components/navigation/navigation';
 import React from 'react';
 import Logo from './components/logo/logo';
+import ImageLinkForm from './components/imageLinkForm/imageLinkForm';
+import Rank from './components/rank/Rank';
+import ParticlesBg from 'particles-bg';
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg type="cobweb" bg={true} color="#FFFFFF"/>
       <Navigation />
       <Logo />
-      {/*}
+      <Rank />
       <ImageLinkForm />
+      {/*}
       <FaceRecognition /> */}
     </div>
   );
