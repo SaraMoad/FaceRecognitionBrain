97b0360f0a9bc09bed07fe87f3bc82ade4ca8ce6
Sara Moad <102942360+SaraMoad@users.noreply.github.com>
2023-02-20 13:07:22 +1100

    Update on face recognition

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -5,19 +5,40 @@ import Logo from './components/logo/logo';
 import ImageLinkForm from './components/imageLinkForm/imageLinkForm';
 import Rank from './components/rank/Rank';
 import ParticlesBg from 'particles-bg';
+import Calarifi from 'clarifai-nodejs-grpc';
 
-function App() {
+
+
+class App extends Component {
+  constructor() {
+  super();
+  this.state = {
+    input: '',
+  }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+    this.setState({imageUrl: this.state.input})
+  }
+
+  render() {
   return (
     <div className="App">
       <ParticlesBg type="cobweb" bg={true} color="#FFFFFF"/>
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
-      {/*}
-      <FaceRecognition /> */}
+      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+      <FaceRecognition imageUrl={this.state.imageUrl}/>
     </div>
+  
   );
+    }
 }
 
 export default App;
